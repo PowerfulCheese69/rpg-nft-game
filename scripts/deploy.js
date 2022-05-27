dd70c296ae9619d29f4875a3faa7c779ae0d52e1
Powerful Cheese <46958595+PowerfulCheese69@users.noreply.github.com>
2022-05-27 12:49:31 +0100

    fixed single NFT deployment issue, now deploys all 3

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -22,11 +22,14 @@ const main = async () => {
     console.log("Contract deployed to:", gameContract.address);
     // We only have three characters.
     // an NFT w/ the character at index 2 of our array.
-    txn = await gameContract.mintCharacterNFT(2);
-    await txn.wait();
+
+      txn = await gameContract.mintCharacterNFT(0);
+      txn = await gameContract.mintCharacterNFT(1);
+      txn = await gameContract.mintCharacterNFT(2);
+      // await txn.wait();
 
     // Get the value of the NFT's URI.
-    let returnedTokenUri = await gameContract.tokenURI(1);
+    let returnedTokenUri = await gameContract.tokenURI();
     console.log("Token URI:", returnedTokenUri);
     };
 
