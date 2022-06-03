5172f1ac993dac137fe2e7194c1fc7442572859e
Powerful Cheese <46958595+PowerfulCheese69@users.noreply.github.com>
2022-06-03 11:13:57 +0100

    Changed skill logic

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -182,7 +182,7 @@ contract MyEpicGame is ERC721 {
     uint256 currentFishingLvl = player.fishing;
     uint256 newFishingLvl = currentFishingLvl+=1; 
 
-    player.fishing +=1;
+    player.fishing = player.fishing + 1;
 
     console.log("\nPlayer w/ character %s has a fishing level of %s.", player.name, player.fishing);
     emit FishingLvlUp(currentFishingLvl,newFishingLvl);
