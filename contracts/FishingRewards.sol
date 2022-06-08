ec9aa1bde538bf368a161b3b196ac35e4fea4db6
Powerful Cheese <46958595+PowerfulCheese69@users.noreply.github.com>
2022-06-08 11:29:37 +0100

    Added TokenURI function

--- contracts/FishingRewards.sol
+++ contracts/FishingRewards.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 
 contract FishingRewards is ERC721, ERC721Burnable, Ownable {
@@ -19,4 +20,18 @@ contract FishingRewards is ERC721, ERC721Burnable, Ownable {
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
     }
+
+ function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
 }
